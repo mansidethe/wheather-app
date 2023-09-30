e90a5f5d4343b8bc31c53f9a2dc6852c5f613901
mansidethe <mansidethe18@gmail.com>
2023-09-30 09:13:33 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from "react";
+import axios from 'axios'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState("Pune");
+  const [weatherDescription,setWheatherDescription] = useState("")
+
+  async function loadWeatherData(){
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}& appid-f652964084c552e8c0492237a3fabd9c`)
+
+    setWeatherData(response.data)
+  }
+
+useEffect(() => {
+  loadWeatherData();
+}, [])
+
+useEffect(()=>{
+  setWheatherDescription("")
+
+ `${weatherData?.weather?.description} ()`
+}, [weatherData])
+
+  return(
+    <div>
+    <h1>Cloud Clustering</h1>
+    <p>City: {weatherData?.name}</p>
+
+    <p>Temperature:{weatherData?.main?.temp-273}tofixed(2)</p>
+
+    <p>Description: {weatherDescription}
+    </p>
+
+    <p>Visibility: {weatherData?.visibility}meter</p>
     </div>
-  );
+  )
 }
-
-export default App;
